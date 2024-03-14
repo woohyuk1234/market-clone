6028f24b24cafd32e906c0058b9970ea6cfc9242
woohyuk1234 <psj4415@naver.com>
2024-03-14 17:02:56 +0900

    💕글쓰기 페이지 기능 구현
    -프론트엔드 글쓰기 페이지 작업
    -백엔드 sqllite 연동해 데이터 업로드 기능구현 완료 했습니다

---
+++ main.py
@@ -0,0 +1,48 @@
+from typing import Annotated
+from fastapi import FastAPI, UploadFile, Form, Response,Depends
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.staticfiles import StaticFiles
+import sqlite3
+
+con = sqlite3.connect('db.db', check_same_thread=False)
+cur = con.cursor()
+app = FastAPI()
+
+@app.post("/items")
+async def create_item(image: UploadFile,
+                      title: Annotated[str, Form()],
+                      price: Annotated[int, Form()],
+                      description: Annotated[str, Form()],
+                      place: Annotated[str, Form()],
+                      insertAt: Annotated[int, Form()]
+                      ):
+    image_bytes = await image.read()
+    cur.execute(f"""
+                INSERT INTO 
+                items(title,image,price,description,place,insertAt)
+                VALUES('{title}','{image_bytes.hex()}',{price},'{description}','{place}',{insertAt})
+                """)
+    con.commit()
+    return '200'
+
+@app.get("/items")
+async def get_items():
+    # 컬럼명도 같이 가져옴
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    rows = cur.execute(f"""
+                       SELECT * from items;
+                       """).fetchall()
+    return JSONResponse(jsonable_encoder(dict(row) for row in rows))
+
+
+@app.get('/images/{item_id}')
+async def get_image(item_id):
+    cur = con.cursor()
+    image_bytes = cur.execute(f"""
+                              SELECT image from items WHERE id={item_id}
+                              """).fetchone()[0]
+    return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
